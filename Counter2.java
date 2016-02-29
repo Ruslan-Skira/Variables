e9c5df9b1352e4cba73153d228aed756c7cde8f0
ruslan <skira.ruslan@gmail.com>
2016-02-29 11:36:17 +0200

    create all task in one class

--- Counter2.java
+++ Counter2.java
@@ -1,13 +1,20 @@
 package NinaTasks;//1. Составьте программу, котораЯ вычисляет сумму чисел от 1 до n.значение n вводится с клавиатуры.
 
+import java.util.Scanner;
+
 public class Counter2 {
+
     public static void main (String [] args){
 
-        Counter c = new Counter();
-        c.Insert();
 
-       int sum = 0;
-        for (int i = 1; i <=c.firstNumber; i++ ){
+        int firstNumber;
+        int sum = 0;
+
+        System.out.println("please enter the first number");
+        Scanner scn = new Scanner(System.in);
+        firstNumber = scn.nextInt();
+
+        for (int i = 1; i <= firstNumber; i++ ){
             System.out.println(i);
             sum = sum + i;
         }
