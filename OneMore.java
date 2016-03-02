666b5185d50576600499a6be3978ef1c55f36666
ruslan <skira.ruslan@gmail.com>
2016-03-02 14:26:16 +0200

    Made package with steps

---
+++ OneMore.java
@@ -0,0 +1,16 @@
+package NinaTasks;
+
+/**
+ * Created by ruslan on 01.03.16.
+ */
+public class OneMore {
+
+    public static void main ( String [] args) {
+        int x = 10;
+        for (int i = 2; i < x; i++) {
+            int z = x;
+            if(){}
+
+        }
+    }
+}
